aa124b372630a08f242f3f3eddf8f0a29f8e7f49
Tobin Yehle <tobinyehle@gmail.com>
2017-04-23 22:27:14 -0600

    update the problem choosing algorithm

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@
 import sys
 import json
 import random
+import time
 
 import praw
 import yagmail
@@ -35,6 +36,10 @@ def init_yag():
     yag.useralias = "robotobo@tobinyehle.com"
     return yag
 
+def get_date():
+    """ Gets the current date in the ISO format. """
+    return time.strftime("%Y-%m-%d")
+
 def latest(reddit):
     """ Get the latest posts """
     posts = list(reddit.subreddit('dailyprogrammer').new(limit=3))
@@ -43,45 +48,53 @@ def latest(reddit):
         print("Challenge numbers do not match: {}".format([post.title for post in posts]))
         return None
     else:
-        number = numbers.pop()
         levels = {post.title.split()[3][1:-1] : post for post in posts}
-        return (levels, number)
+        return levels
 
-def choose_language(address, languages, number):
+def choose_language(address, languages):
     """ Chooses the language for this user.
         Uses the challenge number and email to seed the rng.
     """
-    levels = sorted(languages.keys())
-    all_languages = sorted({l for ls in languages.values() for l in ls})
-    lang_to_level = [(lang, [l for l in levels if lang in languages[l]])
-                     for lang in all_languages]
-    # don't even thing about doing this concurrently
-    random.seed(address + number)
-    pair = choose(lang_to_level)
-    level = choose(pair[1])
-    return (pair[0], level)
+    random.seed(address + get_date())
+    return choose(languages)
+
+def choose_level(users):
+    """ Chooses the level for this week based on a hash of the date """
+    random.seed(get_date())
+    return choose(["Easy", "Intermediate"])
 
 def choose(elems):
     """ Chooses a single element of the given list. """
     return list(elems)[random.randrange(len(elems))]
 
-def send_messages(users, yag, levels, number):
+def send_messages(users, yag, levels):
     """ Send out messages to all the users. """
+    level = choose_level(users)
     for (address, languages) in users.items():
-        (lang, level) = choose_language(address, languages, number)
+        lang = None
+        if len(languages[level]) == 0:
+            lang = "a language of your choice"
+        else:
+            lang = choose_language(address, languages[level])
         send_message(address, yag, level, lang, levels[level])
 
 def send_message(address, yag, level, language, post):
     """ Sends the user a message containing the problem in the given post. """
-    message = """This week you will be doing a problem rated {} in {}!
+    title = post.title.split(']')[-1].strip()
+    date = get_date()
+    subject = "[Weekly Programming Problem] {} in {}".format(title, language)
+    message = """This week you will be doing the {} {} problem in {}!
+                 To push your code to the studio repo (https://github.com/tyehle/programming-studio) put it in this week's folder in a folder with your name (ie: {}/tobin/*.py), or as a single file with your name in it (ie: {}/tobin_code.py).
 
                  This problem's was source is {}.
 
                  {}
-                 """.format(level, language, post.shortlink, post.selftext_html)
-    print("Sending to {}: {}".format(address, message))
-    if address == "tobinyehle@gmail.com":
-        yag.send(to=address, subject="Weekly Programming Problem", contents=message)
+                 """.format(level, title, language, date, date, post.shortlink, post.selftext_html)
+    print("Sending ({}, {}) to {}".format(level, language, address))
+    # if address == "tobinyehle@gmail.com":
+    #     print(subject)
+    #     print(message)
+    yag.send(to=address, subject=subject, contents=message)
 
 def main():
     """ Main function to run if this file is run as a script """
@@ -90,17 +103,16 @@ def main():
         print("No reddit instance")
     else:
         with init_yag() as yag:
-            late = latest(reddit)
-            if late is None:
+            levels = latest(reddit)
+            if levels is None:
                 print("Could not retrieve challenges")
             else:
-                (levels, number) = late
                 with open("users.json", "r") as user_file:
                     users = json.load(user_file)
                     if users is None:
                         print("Could not read user file")
                     else:
-                        send_messages(users, yag, levels, number)
+                        send_messages(users, yag, levels)
                         print("Messages sent")
 
 if __name__ == "__main__":
