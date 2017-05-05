1782c45113b14ee65b2540ac61d63dfafe8f6a90
Tobin Yehle <tobinyehle@gmail.com>
2017-05-04 23:46:49 -0600

    add type annotations and update code to grab challenges

--- scraper.py
+++ scraper.py
@@ -5,6 +5,8 @@ import json
 import random
 import time
 
+from typing import List, Dict, Optional, Callable, Iterable, TypeVar
+
 import praw
 import yagmail
 
@@ -12,11 +14,17 @@ VERSION = "v0.1"
 ID = "dailyprogrammer-scraper"
 AUTHOR = "/u/ToboRoboLoco"
 
-def user_agent():
+# Yag = yagmail.yagmail.SMTP
+# Reddit = praw.reddit.Reddit
+# Post = praw.models.reddit.submission.Submission
+A = TypeVar("A")
+B = TypeVar("B")
+
+def user_agent() -> str:
     """ Build the user agent string """
     return "{}:{}:{} by {}".format(sys.platform, ID, VERSION, AUTHOR)
 
-def init_reddit():
+def init_reddit() -> Optional[praw.reddit.Reddit]:
     """ Gets a praw reddit instance """
     client_info = None
     with open("client-info.json", mode='r') as info_handle:
@@ -30,48 +38,63 @@ def init_reddit():
     else:
         return praw.Reddit(user_agent=user_agent(), **client_info)
 
-def init_yag():
+def init_yag() -> yagmail.yagmail.SMTP:
     """ Gets a yagmail instance for sending emails. """
     yag = yagmail.SMTP("tobin.spam@gmail.com")
     yag.useralias = "robotobo@tobinyehle.com"
     return yag
 
-def get_date():
+def get_date() -> str:
     """ Gets the current date in the ISO format. """
     return time.strftime("%Y-%m-%d")
 
-def latest(reddit):
+def group_by(iterable: Iterable[A], key_func: Callable[[A], B]) -> Dict[B, List[A]]:
+    """ Groups an iterable by equality of keys. """
+    out = dict() # type: Dict[B, List[A]]
+    for item in iterable:
+        key = key_func(item)
+        if key in out:
+            out[key].append(item)
+        else:
+            out[key] = [item]
+    return out
+
+def latest(reddit: praw.reddit.Reddit) -> Optional[Dict[str, praw.models.reddit.submission.Submission]]:
     """ Get the latest posts """
-    posts = list(reddit.subreddit('dailyprogrammer').new(limit=3))
-    numbers = {post.title.split()[2][1:] for post in posts}
-    if len(numbers) != 1:
-        print("Challenge numbers do not match: {}".format([post.title for post in posts]))
+    posts = list(reddit.subreddit('dailyprogrammer').new(limit=5))
+    grouped = group_by(posts, lambda post: post.title.split()[2][1:])
+    only3 = filter(lambda posts: len(posts) == 3, grouped.values())
+    result = max(only3,
+                 key=lambda posts: int(posts[0].title.split()[2][1:]),
+                 default=[])
+    if len(result) != 3:
+        print("Could not get matching challenges from: {}".format([post.title for post in posts]))
         return None
     else:
         levels = {post.title.split()[3][1:-1] : post for post in posts}
         return levels
 
-def choose_language(address, languages):
+def choose_language(address: str, languages: List[str]) -> str:
     """ Chooses the language for this user.
         Uses the challenge number and email to seed the rng.
     """
     random.seed(address + get_date())
     return choose(languages)
 
-def choose_level(users):
+def choose_level(users: Dict[str, Dict[str, List[str]]]) -> str:
     """ Chooses the level for this week based on a hash of the date """
     all_levels = [level
                   for levels in users.values()
                   for (level, languages) in levels.items()
                   for _ in languages]
     random.seed(get_date())
-    return choose(all_levels)
+    return choose(sorted(all_levels))
 
-def choose(elems):
+def choose(elems: List[A]) -> A:
     """ Chooses a single element of the given list. """
-    return list(elems)[random.randrange(len(elems))]
+    return elems[random.randrange(len(elems))]
 
-def send_messages(users, yag, levels):
+def send_messages(users: Dict[str, Dict[str, List[str]]], yag: yagmail.yagmail.SMTP, levels: Dict[str, praw.models.reddit.submission.Submission]) -> None:
     """ Send out messages to all the users. """
     level = choose_level(users)
     for (address, languages) in users.items():
@@ -82,25 +105,33 @@ def send_messages(users, yag, levels):
             lang = choose_language(address, languages[level])
         send_message(address, yag, level, lang, levels[level])
 
-def send_message(address, yag, level, language, post):
+def send_message(address: str, yag: yagmail.yagmail.SMTP, level: str, language: str, post: praw.models.reddit.submission.Submission) -> None:
     """ Sends the user a message containing the problem in the given post. """
     title = post.title.split(']')[-1].strip()
     date = get_date()
     subject = "[Weekly Programming Problem] {} in {}".format(title, language)
+    push_instructions = ("To push your code to the studio repo "+
+                         "(https://github.com/tyehle/programming-studio) put "+
+                         "it in this week's folder in a folder with your name "+
+                         "(ie: {date}/tobin/*.py), or as a single file with "+
+                         "your name in it (ie: {date}/tobin_code.py)."
+                        ).format(date=date)
     message = """This week you will be doing the {} {} problem in {}!
-                 To push your code to the studio repo (https://github.com/tyehle/programming-studio) put it in this week's folder in a folder with your name (ie: {}/tobin/*.py), or as a single file with your name in it (ie: {}/tobin_code.py).
+                 {}
 
-                 This problem's was source is {}.
+                 This problem's source is {}.
 
                  {}
-                 """.format(level, title, language, date, date, post.shortlink, post.selftext_html)
+                 """.format(level, title, language,
+                            push_instructions,
+                            post.shortlink, post.selftext_html)
     print("Sending ({}, {}) to {}".format(level, language, address))
     # if address == "tobinyehle@gmail.com":
     #     print(subject)
     #     print(message)
-    # yag.send(to=address, subject=subject, contents=message)
+    yag.send(to=address, subject=subject, contents=message)
 
-def main():
+def main() -> int:
     """ Main function to run if this file is run as a script """
     reddit = init_reddit()
     if reddit is None:
