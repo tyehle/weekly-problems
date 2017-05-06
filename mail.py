9cdec32ae9f62f1bdce0f72399a3fe15b56495f5
Tobin Yehle <tobinyehle@gmail.com>
2017-05-06 11:20:44 -0600

    interface to gmail done

--- mail.py
+++ mail.py
@@ -3,8 +3,11 @@
 import os
 import json
 import argparse
-import base64
+import datetime
 
+import base64
+import email
+from email.mime.text import MIMEText
 import httplib2
 from apiclient import discovery
 from oauth2client import client
@@ -12,10 +15,10 @@ from oauth2client import tools
 from oauth2client.file import Storage
 
 # If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/gmail-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/gmail.readonly'
+# at ~/.credentials/gmail-python.json
+SCOPES = 'https://www.googleapis.com/auth/gmail.modify'
 CLIENT_SECRET_FILE = 'mail_secret.json'
-APPLICATION_NAME = 'Gmail API Python Quickstart'
+APPLICATION_NAME = 'Weekly Programming Problem'
 
 
 def get_credentials(flags):
@@ -32,7 +35,7 @@ def get_credentials(flags):
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
     credential_path = os.path.join(credential_dir,
-                                   'gmail-python-quickstart.json')
+                                   'gmail-python.json')
 
     store = Storage(credential_path)
     credentials = store.get()
@@ -43,54 +46,128 @@ def get_credentials(flags):
         print('Storing credentials to ' + credential_path)
     return credentials
 
-def get_contents(msg):
-    """ Gets the contents of a message. """
-    payload = msg["payload"]
+def get_message(service, user_id, msg_id):
+    """ Gets a message using the given client. """
+    result = service.users().messages().get(userId=user_id,
+                                            id=msg_id,
+                                            format="raw").execute()
+    msg_str = base64.urlsafe_b64decode(result["raw"].encode("ASCII"))
+    return email.message_from_bytes(msg_str)
 
-    if payload["mimeType"] == "multipart/alternative":
-        parts = payload["parts"]
-        by_type = {part["mimeType"]: part for part in parts}
-        if "text/plain" in by_type:
-            contents = base64.urlsafe_b64decode(by_type["text/plain"]["body"]["data"]).decode()
-            return contents
-        else:
-            print("No plain text type found!")
-            return None
+def get_text_content(msg):
+    """ Gets the plain text content of a MIME message if it exists.
+        If there is no plain text content then the result is None.
+    """
+    if msg.get_content_type() == "text/plain":
+        return msg.get_payload(decode=True).decode(msg.get_content_charset())
+    elif msg.get_content_type() == "multipart/alternative":
+        for part in msg.get_payload():
+            content = get_text_content(part)
+            if content is not None:
+                return content
+        return None
     else:
-        print("Unknown message type: {}".format(payload["mimeType"]))
         return None
 
-def lookup(name, store, default=None):
-    """ Looks up the name in the given store.
-        If the name is not found then the default value is returned.
-        If the no default is given then None is returned.
+def make_message(body, subject, recipient, sender, alias=None, **other_headers):
+    """ Builds a raw mime message from the given information. """
+    message = MIMEText(body)
+    message["Subject"] = subject
+    message["To"] = recipient
+    if alias is not None:
+        message["From"] = "{} <{}>".format(alias, sender)
+    else:
+        message["From"] = sender
+    for key, value in other_headers.items():
+        message[key] = value
+    return message
+
+def send(service, user_id, msg, thread_id=None):
+    """ Sends the given message. """
+    as_bytes = {"raw": base64.urlsafe_b64encode(msg.as_bytes()).decode("ASCII")}
+    if thread_id is not None:
+        as_bytes["threadId"] = thread_id
+    print("Sending {}".format(as_bytes))
+    result = service.users().messages().send(userId=user_id, body=as_bytes).execute()
+    print("Sent message with id: {}".format(result["id"]))
+    return result
+
+def trash(service, user_id, msg_id):
+    """ Puts the given message in the trash. """
+    return service.users().messages().trash(userId=user_id, id=msg_id).execute()
+
+def list_messages(service, user_id, labels="INBOX"):
+    """ Gets a messages for the given user.
+        By default gets messages in the inbox.
     """
-    return next((x["value"] for x in store if x["name"] == name), default)
+    return service.users().messages().list(userId=user_id, labelIds=labels).execute()
+
+def quote(message):
+    """ Quote the text from a message for a reply. """
+    date = datetime.datetime.strptime(message["Date"], "%a, %d %b %Y %X %z")
+    date_string = date.strftime("%a, %b %d, %Y at %I:%M %p")
+
+    content = get_text_content(message)
+    quoted = "\r\n".join("> " + line for line in content.split("\r\n"))
+
+    return "On {} {} wrote:\r\n\r\n{}\r\n".format(date_string, message["From"], quoted)
+
+def make_reply(message, body, alias=None):
+    """ Create a message in reply to another message. """
+    orig_subject = message["Subject"]
+    subject = orig_subject if "Re: " in orig_subject else "Re: " + orig_subject
+
+    refs = message.get("References", failobj="") + " " + message["Message-ID"]
+    refs = refs.strip() # remove the leading space if the get failed
+
+    recipient = message.get("Reply-To", failobj=message["From"])
+
+    return make_message(body="{}\r\n\r\n{}".format(body, quote(message)),
+                        subject=subject,
+                        recipient=recipient,
+                        sender="tobin.spam@gmail.com",
+                        alias=alias,
+                        **{"In-Reply-To": message["Message-ID"],
+                           "References": refs})
 
 def main():
-    """Shows basic usage of the Gmail API.
-
-    Creates a Gmail API service object and outputs a list of label names
-    of the user's Gmail account.
+    """ Creates a Gmail API service object and outputs a list of label names
+        of the user's Gmail account.
     """
     flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
     credentials = get_credentials(flags)
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('gmail', 'v1', http=http)
 
-    messages = service.users().messages().list(userId='me', labelIds="INBOX").execute()
-    for msg in messages.get("messages", []):
-        results = service.users().messages().get(userId="me", id=msg["id"], format="full").execute()
-        headers = results["payload"]["headers"]
-        subject = lookup("Subject", headers)
-        recipient = lookup("To", headers)
-        sender = lookup("From", headers)
-        print("\n\n---------- MESSAGE ----------\nid: {}\nsubject: {}\nto: {}\nfrom: {}\n".format(message_id, subject, recipient, sender))
-        print(get_contents(results))
-        # print(json.dumps(results, indent=2))
+    out = make_message(body="Hey look I changed my picture!",
+                       subject="Test Message",
+                       recipient="tobinyehle@gmail.com",
+                       sender="tobin.spam@gmail.com",
+                       alias="RoboTobo")
+    # send(service, "me", out)
 
-    # to_send = {}
-    # service.users().
+    messages = list_messages(service, user_id='me')
+    for msg in messages.get("messages", []):
+        message = get_message(service, "me", msg["id"])
+        content = get_text_content(message)
+
+        if "Delete" in message["subject"]:
+            print("Found a message to delete!")
+            print(message["subject"])
+            print("deleting message with id: " + msg["id"])
+            # deleted = trash(service, "me", msg["id"])
+            # print(deleted)
+
+        if "Modify" in message["subject"]:
+            print("found a modify message!")
+            reply = make_reply(message, "This service is not yet implemented", alias="RoboTobo")
+            print(reply)
+            # send(service, "me", reply, msg["threadId"])
+
+        print(content)
+        print(msg)
+        headers = {k: message[k] for k in message.keys()}
+        print("Message details:\n{}".format(json.dumps(headers, indent=2)))
 
 
 if __name__ == '__main__':
