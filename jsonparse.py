0c9cf67f9bd746c79176bdf3b1a89cc77717fa15
Tobin Yehle <tobinyehle@gmail.com>
2017-05-17 11:34:28 -0600

    update jsonparse to be curried

--- jsonparse.py
+++ jsonparse.py
@@ -10,7 +10,7 @@ from result import Result, Err, Ok, map_m
 
 A = TypeVar("A")
 # Generic type aliases need the new typing module
-# Parser = Callable[[Any], Result[str, A]]
+Parser = Callable[[Any], Result[str, A]]
 
 def run_parser(raw: str, parser: Callable[[Any], Result[str, A]]) -> Result[str, A]:
     """ Run a parser on some raw string. """
@@ -20,43 +20,44 @@ def run_parser(raw: str, parser: Callable[[Any], Result[str, A]]) -> Result[str,
     except json.decoder.JSONDecodeError as exn:
         return Err(str(exn))
 
-def dict_parser(obj: Any,
-                inner_parser: Callable[[Any], Result[str, A]]) -> Result[str, Dict[str, A]]:
+def dict_parser(inner: "Parser[A]") -> "Parser[Dict[str, A]]":
     """ Parse a dictionary. """
     def check_item(item: Tuple[Any, Any]) -> Result[str, A]: # pylint: disable=C0111
         key, value = item
-        return str_parser(key).bind(lambda _: inner_parser(value))
+        return str_parser(key).bind(lambda _: inner(value))
+    def out(obj: Any) -> Result[str, Dict[str, A]]: # pylint: disable=C0111
+        if isinstance(obj, dict):
+            return map_m(check_item, obj.items()).fmap(lambda _: cast(Dict[str, A], obj))
+        else:
+            return bad_type(dict, obj)
+    return out
 
-    if isinstance(obj, dict):
-        return map_m(check_item, obj.items()).fmap(lambda _: cast(Dict[str, A], obj))
-    else:
-        return bad_type(dict, obj)
-
-def list_parser(obj: Any,
-                inner_parser: Callable[[Any], Result[str, A]]) -> Result[str, List[A]]:
+def list_parser(inner: "Parser[A]") -> "Parser[List[A]]":
     """ Parse a list. """
-    if isinstance(obj, list):
-        return map_m(inner_parser, obj)
-    else:
-        return bad_type(list, obj)
+    def out(obj: Any) -> Result[str, List[A]]: # pylint: disable=C0111
+        if isinstance(obj, list):
+            return map_m(inner, obj)
+        else:
+            return bad_type(list, obj)
+    return out
 
 def bad_type(expected: Any, result: Any) -> Result[str, A]:
     """ Fail with a type error message. """
     return Err("Expecting {}, but got {} in '{}'".format(expected, result.__class__, result))
 
-def leaf_parser(t: type) -> Callable[[Any], Result[str, A]]:
+def leaf_parser(t: type) -> Parser:
     """ Generic parser for json leaves. """
-    def parser(obj: Any) -> Result[str, A]: # pylint: disable=C0111
+    def parser(obj: Any) -> Result[str, Any]: # pylint: disable=C0111
         if isinstance(obj, t):
-            return Ok(cast(A, obj))
+            return Ok(obj)
         else:
             return bad_type(t, obj)
     return parser
 
-str_parser = leaf_parser(str) # type: Callable[[Any], Result[str, str]]
-int_parser = leaf_parser(int) # type: Callable[[Any], Result[str, int]]
-float_parser = leaf_parser(float) # type: Callable[[Any], Result[str, float]]
-bool_parser = leaf_parser(bool) # type: Callable[[Any], Result[str, bool]]
+str_parser = leaf_parser(str) # type: Parser[str]
+int_parser = leaf_parser(int) # type: Parser[int]
+float_parser = leaf_parser(float) # type: Parser[float]
+bool_parser = leaf_parser(bool) # type: Parser[bool]
 
 def none_parser(obj: Any) -> Result[str, None]:
     """ Parse none. """
