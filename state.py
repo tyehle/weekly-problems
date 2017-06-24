ac347459c6792909b518a636052c0afa1d70a2a8
Tobin Yehle <tobinyehle@gmail.com>
2017-06-24 16:35:46 -0600

    change back to address dict from user list

--- state.py
+++ state.py
@@ -27,58 +27,53 @@ class User(object):
     """A user in the system.
 
     Attributes:
-        address: The user's email address
         langs: The set of languages the user has specified
         vetoed: If the user has vetoed this week's challenge
         last_lang: The last language the user was assigned
     """
     def __init__(self,
-                 address: str,
                  langs: Dict[str, List[str]],
                  vetoed: bool,
                  last_lang: Optional[str]
-                 ) -> None:
+                ) -> None:
         """Populates all the fields of a new user.
 
         Args:
-            address: The user's email address
             langs: The set of languages the user has specified
             vetoed: If the user has vetoed this week's challenge
             last_lang: The last language the user was assigned
         """
-        self.address = address
         self.langs = langs
         self.vetoed = vetoed
         self.last_lang = last_lang
 
     def __repr__(self) -> str:
         """String representation of the user."""
-        return "User(address={}, langs={}, vetoed={}, last_lang={})".format(
-            repr(self.address),
+        return "User(langs={}, vetoed={}, last_lang={})".format(
             repr(self.langs),
             repr(self.vetoed),
             repr(self.last_lang)
         )
 
 
-def load_state() -> Result[str, List[User]]:
-    """Load the list of users from disk.
+def load_state() -> Result[str, Dict[str, User]]:
+    """Load the users from disk.
 
     Returns:
-        The list of users on the disk, or Err if there was a problem during loading.
+        A map from email address to the user, or an Err if there was a problem during loading.
     """
-    user_parser = field("address", str_parser, lambda address:
-                  field("langs", dict_parser(list_parser(str_parser)), lambda langs:
+    user_parser = field("langs", dict_parser(list_parser(str_parser)), lambda langs:
                   field("vetoed", bool_parser, lambda vetoed:
                   field("last_lang", optional_parser(str_parser), lambda last_lang:
-                  done(User(address, langs, vetoed, last_lang))))))
-    return run_parser_file(STATE_PATH, list_parser(user_parser))
+                  done(User(langs, vetoed, last_lang))))) # pylint: disable=undefined-variable
+    return run_parser_file(STATE_PATH, dict_parser(user_parser))
 
 
-def save_state(users: List[User]) -> None:
-    """Saves a list of users to disk, overwriting the any data currently stored.
+def save_state(users: Dict[str, User]) -> None:
+    """Saves a map of addresses to users to disk, overwriting any data currently stored.
 
     Args:
-        users: The list of users to write
+        users: The map of users to write
     """
-    json.dump([user.__dict__ for user in users], open(STATE_PATH, "w"), indent=2)
+    raw = {address: user.__dict__ for (address, user) in users.items()}
+    json.dump(raw, open(STATE_PATH, "w"), indent=2)
